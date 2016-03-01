30190754b2404ef0fd1fec8f437c0b49705754e2
Reuben Binns <reunns@Reubens-MacBook.lan>
2016-03-01 13:52:25 +0000

    addedd scraper for play store permissions dara

---
+++ playstore_permissions.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+driver = webdriver.Firefox()
+
+driver.get("https://play.google.com/store/apps/details?id=info.blockchain.merchant")
+
+driver.find_element_by_css_selector("button.content.id-view-permissions-details.fake-link").click()
+
+# for whole page
+htmlElem = driver.find_element_by_tag_name('html')
+print htmlElem.text
+
+# OR for just the permissions text
+
+permdeets = driver.find_element_by_css_selector("div.permission-buckets.id-permission-buckets")
+
+permdeets.text
