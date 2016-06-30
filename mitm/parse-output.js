a77041a726b92083842cdd07626db69820d4b901
electronic Max <electronic@gmail.com>
2016-06-30 18:22:22 +0100

    updated parse

--- mitm/parse-output.js
+++ mitm/parse-output.js
@@ -8,22 +8,23 @@ var parse = require('csv-parse/lib/sync'),
 	qs = require('querystring'),
 	config = JSON.parse(fs.readFileSync('./config.json'));
 
-console.log("Parsing file ", config.input);
-data = parse(fs.readFileSync(config.input).toString());
-headers = data[0];
-data = data.slice(1);
-data = data.map((x) => _.zipObject(headers,x));
-hosts = _(data).map((x)=>x.host).uniq().value();
-console.log('hosts : ', hosts);
-
-// sum up counts
-counts = _(data).reduce((y,x) => { y[x.host] = y[x.host] ? y[x.host] + 1 : 1; return y; },{})
-console.log('counts: ', counts);
-
-_(data).map((x) => console.log(x.params))
-
-// decodes the urlstrings of the gets
-var decode_urls = (datas) =>  {
+var loadFile = (fname) => {
+	console.log("Parsing file ", fname);
+	data = parse(fs.readFileSync(fname).toString());
+	headers = data[0];
+	data = data.slice(1);
+	data = data.map((x) => _.zipObject(headers,x));
+	// for giggles
+	// hosts = _(data).map((x)=>x.host).uniq().value();	
+	// counts = _(data).reduce((y,x) => { y[x.host] = y[x.host] ? y[x.host] + 1 : 1; return y; },{})
+	// console.log('counts: ', counts);
+	// _(data).map((x) => console.log(x.params))
+	return data;
+}, loadDir = () => {
+	// loads all of the data in the specified directory
+	var srcdir = config.inputdir;
+	return fs.readdirSync(srcdir).reduce((arr,fname) => arr.concat(loadFile([srcdir,fname].join('/'))), []);
+}, decode_urls = (datas) =>  {
 	return datas.map((x) => {
 		var url = decodeURIComponent(x.url);	 
 		if (url.indexOf('?') >= 0) { 
@@ -34,7 +35,6 @@ var decode_urls = (datas) =>  {
 	}).filter((x)=>x);
 };
 
-exports.data = data;
-exports.counts = counts;
 exports.decode_urls = decode_urls;
+exports.load = loadDir;
 
