26c7f7c048ff8b8d4aa21ce4ab38aae759c5069c
RDBinns <reuben.binns@cs.ox.ac.uk>
2016-07-22 11:08:38 +0100

    added host2co changes

--- companydata/host2co.js
+++ companydata/host2co.js
@@ -1,10 +1,17 @@
 var parser = require('parse-whois');
 var whois = require('node-whois');
+var openCorporates = require('opencorporates')()
+
+host = 'xiaomi.com'
+
+company = ''
  
-whois.lookup('github.com', function(err, data){
+whois.lookup(host, function(err, data){
 	if (err) throw err;
- 
-	console.log(data);
- 
-	console.log(parser.parseWhoIsData(data));
+	parsed = parser.parseWhoIsData(data);
+	company = parsed[14].value
+	console.log(company)
+	openCorporates.companies.search('xiamoi Inc', function(err, res){
+	console.log(JSON.stringify(res, null, 2))
+})
 });
