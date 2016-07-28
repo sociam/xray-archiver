e3a34b4face99a54688cde3e5c9fbd3dbb194ba4
electronic Max <electronic@gmail.com>
2016-07-28 20:32:00 +0100

    progress

--- prototypes/js/p1.js
+++ prototypes/js/p1.js
@@ -30,16 +30,17 @@ angular.module('dci', ['ui.router', 'ngAnimate', 'ngTouch', 'ngSanitize'])
 			resolve: {
 				pitypes:($http) => $http.get('../mitm_out/pi_by_host.json').then((x) => x.data),
 				hosts: ($http) => $http.get('../mitm_out/host_by_app.json').then((x) => x.data),
-				companydetails: ($http) => $http.get('../mitm_out/company_details.json').then((x) => x.data),
+				details: ($http) => $http.get('../mitm_out/company_details.json').then((x) => x.data),
 				data: ($http) => $http.get('../mitm_out/data_all.json').then((x) => x.data)
 			},
-			controller:function($scope, pitypes, hosts, companydetails, data, $stateParams) {
+			controller:function($scope, pitypes, hosts, details, data, $stateParams) {
 				console.log('boxdci stateparams', $stateParams);
 				console.log('got relevant ', data.length);
 				
-				data = data.filter((x) => x.app == $stateParams.app);
+				data = data.filter((x) => x.app === $stateParams.app);
 				var app = $scope.app = $stateParams.app,
 					appcompany = $scope.appcompany = data[0].company,
+					id2names = data.reduce((a,x) => { a[x.id] = x.company; return a; }, {}),
 					hTc = $scope.hTc = data.reduce((r,a) => {
 						if (a.host_company) { 
 							r[a.host] = a.host_company; 
@@ -48,24 +49,24 @@ angular.module('dci', ['ui.router', 'ngAnimate', 'ngTouch', 'ngSanitize'])
 						return r;
 					}, {}),
 					hTh = $scope.hTh = data.reduce((r,a) => {
+						// host -> 2ld
 						if (a.host_2ld) { 
 							r[a.host] = a.host_2ld;
 						}
 						return r;
 					}, {}),
-					isAd = (company) => {
-						return company && 
-							company !== appcompany && // don't consider the first party an ad (e.g. google)
-							companydetails[company] && 
-							companydetails[company].type.indexOf('advert')>=0;
+					matchCompany = (x) => (x || '').toLowerCase() === appcompany.toLowerCase(),
+					isAd = (id) => {
+						return id && 
+							_.some([id2names[id], id].map(matchCompany)) && 
+							details[id] && details[id].typetag && details[id].typetag.indexOf('advert') >= 0;
 					},
 					recompute = () => {
 						var apphosts = _(hosts[$scope.app]).pickBy((val) => val > $scope.threshold).keys().value();
 						// next we wanna group together all the pi_types, and consolidate around company
 						console.info('threshold', $scope.threshold, 'apphosts', apphosts.length);
 						$scope.company2pi = apphosts.reduce((r,host) => {
-							var company = hTc[host],
-								host_pis = pitypes[host] || [];
+							var company = hTc[host], host_pis = pitypes[host] || [];
 							if (!company) { 
 								var mfirst = hTh[host].match(/^([^\.]+)\./);
 								if (mfirst) { 
@@ -78,7 +79,8 @@ angular.module('dci', ['ui.router', 'ngAnimate', 'ngTouch', 'ngSanitize'])
 							return r;
 						}, {});
 
-						$scope.appcompany2pi = _.pickBy($scope.company2pi, (pis, company) => company === appcompany);
+						// each of the boxes
+						$scope.appcompany2pi = _.pickBy($scope.company2pi, (pis, company) => matchCompany(company));
 						$scope.ad2pi = _.pickBy($scope.company2pi, (pis, company) => !$scope.appcompany2pi[company] && isAd(company));
 						$scope.non2pi = _.pickBy($scope.company2pi, (pis, company) => !$scope.appcompany2pi[company] && !isAd(company));
 					};
@@ -93,6 +95,7 @@ angular.module('dci', ['ui.router', 'ngAnimate', 'ngTouch', 'ngSanitize'])
 				$scope.hosts = hosts;
 				$scope.data = data;
 				$scope.pitypes = pitypes;
+				$scope.details = details;
 				window._s = $scope;
 			}
 		});
