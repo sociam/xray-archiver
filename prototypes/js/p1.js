6ac948b53186a40df82127f9fe7b2a6625262124
electronic Max <electronic@gmail.com>
2016-08-05 15:42:20 +0100

    table progress

--- prototypes/js/p1.js
+++ prototypes/js/p1.js
@@ -23,9 +23,8 @@ angular.module('dci', ['ui.router', 'ngAnimate', 'ngTouch', 'ngSanitize'])
 				$scope.apps = _.uniq(data.map((x) => x.app));
 			}
 		});
-
-		$stateProvider
-		  .state('dci', {
+		// base dci state
+		$stateProvider.state('dci', {
 		  	url: '/dci?app',
 		  	templateUrl:'tmpl/view.html',
 		  	resolve: {
@@ -48,156 +47,7 @@ angular.module('dci', ['ui.router', 'ngAnimate', 'ngTouch', 'ngSanitize'])
 					'dci.table' : 'table'
 				}[$state.$current.toString()];
 			}
-		  }).state('dci.box', {
-			url: '/box',
-			templateUrl: 'tmpl/box-dci.html',
-			resolve: {
-				pitypes:($http) => $http.get('../mitm_out/pi_by_host.json').then((x) => x.data),
-				hosts: ($http) => $http.get('../mitm_out/host_by_app.json').then((x) => x.data),
-				details: ($http) => $http.get('../mitm_out/company_details.json').then((x) => x.data),
-				data: ($http) => $http.get('../mitm_out/data_all.json').then((x) => x.data)
-			},
-			controller:function($scope, pitypes, hosts, details, data, $stateParams) {
-				console.log('boxdci stateparams', $stateParams);
-				console.log('got relevant ', data.length);
-				$scope.$parent.mode = 'box';
-
-				// $scope.apps = _.uniq(data.map((x) => x.app));
-				data = $scope.data = data.filter((x) => x.app === $stateParams.app);
-				// console.log('before filter ', data.length);
-				// data = $scope.data = data.filter((x) => ((details[x.host_company] || {}).typetag || '').indexOf('ignore') < 0);
-				// console.log('after filter ', data.length);				
-
-				if (!data.length) { $scope.error = 'no data for app ' + $stateParams.app; }
-
-				var app = $scope.app = $stateParams.app,
-					appcompany = $scope.appcompany = data[0].company,
-					id2names = $scope.id2names = _.keys(details).reduce((a,id) => { 
-						a[id] = details[id].company; return a; 
-					}, {}),
-					hTc = $scope.hTc = data.reduce((r,a) => {
-						if (a.host_company) { 
-							r[a.host] = a.host_company; 
-							r[a.host_2ld] = a.host_company;
-						}
-						return r;
-					}, {}),
-					hTh = $scope.hTh = data.reduce((r,a) => {
-						// host -> 2ld
-						if (a.host_2ld) { 
-							r[a.host] = a.host_2ld;
-						} else { console.error('warning no 2ld ', a.host); }
-						return r;
-					}, {}),
-					checkSize = () => {},
-					matchCompany = (x) => appcompany && ((x || '').toLowerCase() === appcompany.toLowerCase()),
-					isType = $scope.isType = (id, type) => id && 
-						details[id] && 
-						details[id].typetag && 
-						details[id].typetag.indexOf(type) >= 0,
-					getName = $scope.getName = (id) => details[id] && details[id].company || id,
-					is3rdPartyType = $scope.is3rdPartyType = (id, type) => isType(id,type) &&
-							!_.some([id2names[id], id].map(matchCompany)),
-					recompute = () => {
-						var apphosts = _(hosts[$scope.app]).pickBy((val) => val > $scope.threshold).keys().value();
-						// next we wanna group together all the pi_types, and consolidate around company
-						// console.info('threshold', $scope.threshold, 'apphosts', apphosts.length);
-						$scope.company2pi = apphosts.reduce((r,host) => {
-							var company = hTc[host], 
-								host_pis = pitypes[host] || [];
-							if (!company) { 
-								console.log('hth ', host, hTh[host]);
-								var mfirst = hTh[host].match(/^([^\.]+)\./);
-								if (mfirst) { 
-									company = mfirst[1]; 
-								} else {
-									console.error('no company for host ', host); return r; 
-								}
-							} 
-							r[company] = _.union(r[company] || [], host_pis);
-							return r;
-						}, {});
-
-						// each of the boxes
-						$scope.categories = [
-							{
-								label:'app publisher',
-								class:'app-publisher',
-								companies: _.pickBy($scope.company2pi, (pis, company) => 
-									matchCompany(company))
-							},
-							{
-								label:'app functionality',
-								class:'app-functionality',								
-								companies: _.pickBy($scope.company2pi, (pis, company) => 
-										!isType(company, 'ignore') && 
-										!isType(company, 'platform') && 
-										is3rdPartyType(company,'app'))
-							},
-							{
-								label:'marketing',
-								class:'marketing',								
-								companies: _.pickBy($scope.company2pi, (pis, company) => 
-										!isType(company, 'ignore') && 
-										!isType(company, 'platform') && 
-										is3rdPartyType(company,'marketing'))
-							},
-							{
-								label:'usage tracking',
-								class:'usage',								
-								companies: _.pickBy($scope.company2pi, (pis, company) => 
-										!isType(company, 'ignore') && 
-										!isType(company, 'platform') && 
-										is3rdPartyType(company, 'usage'))
-							},
-							{
-								label:'payments',
-								class:'payments',								
-								companies: _.pickBy($scope.company2pi, (pis, company) => 
-										!isType(company, 'ignore') && 
-										!isType(company, 'platform') && 
-										is3rdPartyType(company, 'payments'))
-							},
-							{
-								label:'security',
-								class:'security',								
-								companies: _.pickBy($scope.company2pi, (pis, company) => 
-										!isType(company, 'ignore') && 
-										!isType(company, 'platform') && 
-										is3rdPartyType(company, 'security'))
-							},							
-							{
-								label:'unknown',
-								class:'other',
-								companies: _.pickBy($scope.company2pi, (pis, company) => 
-									!matchCompany(company) &&							
-									!isType(company, 'ignore') && 							
-									!isType(company, 'app') && 							
-									!isType(company,'marketing') &&
-									!isType(company, 'platform') && 							
-									!isType(company, 'usage') &&
-									!isType(company, 'payments') &&
-									!isType(company, 'security')
-								)
-							}
-						];
-						checkSize();
-					};
-
-				if (!appcompany) { $scope.error = 'Captured data for ' + app + ' is in old data format without company field'; }
-				if (!hosts[$scope.app]) { $scope.error = 'No hosts known for app'; }
-
-				$scope.size = (l) => _.keys(l).length;
-				$scope.threshold = 0;
-				$scope.$watch('threshold', () => { if ($scope.threshold!==undefined) { recompute(); }});
-
-				$scope.hosts = hosts;
-				$scope.data = data;
-				$scope.pitypes = pitypes;
-				$scope.details = details;
-				window._s = $scope;
-			}
-		});
+		  });
 	}).component('companyInfo', {
 	  templateUrl: 'tmpl/company-info.html',
 	  replace:true,
--- prototypes/js/table.js
+++ prototypes/js/table.js
@@ -1,86 +1,7 @@
+/* global angular, _, jQuery, Backbone */
+console.log('table js');
 angular.module('dci')
-	.service('utils', { 
-		var utils = {
-			makeId2Names:(details) => _.keys(details).reduce((a,id) => { a[id] = details[id].company; return a; }, {}),
-			makeHTC:(data) => data.reduce((r,a) => {
-				if (a.host_company) { 
-					r[a.host] = a.host_company; 
-					r[a.host_2ld] = a.host_company;
-				}
-				return r;
-			}, {}),
-			makeHTH:(data) => data.reduce((r,a) => {
-				// host -> 2ld
-				if (a.host_2ld) { 
-					r[a.host] = a.host_2ld;
-				} else { console.error('warning no 2ld ', a.host); }
-				return r;
-			}, {}),
-			isType: (details, id, type) => id && 
-				details[id] && 
-				details[id].typetag && 
-				details[id].typetag.indexOf(type) >= 0,
-			matchCompany : (appcompany, x) => appcompany && 
-				((x || '').toLowerCase() === appcompany.toLowerCase()),
-			is3rdPartyType: (details, id, type) => utils.isType(details,id,type) && 
-				!_.some([id2names[id], id].map(utils.matchCompany)),
-			makeCompany2pi: (data, hosts, pitypes, threshold) => {
-				var apphosts = _(hosts[$scope.app]).pickBy((val) => val > threshold || 0).keys().value(),
-					hTh = utils.makeHTH(data),
-					hTc = utils.makeHTC(data);
-				// next we wanna group together all the pi_types, and consolidate around company
-				// console.info('threshold', $scope.threshold, 'apphosts', apphosts.length);
-				return apphosts.reduce((r,host) => {
-					var company = hTc[host], 
-						host_pis = pitypes[host] || [];
-					if (!company) { 
-						var mfirst = hTh[host].match(/^([^\.]+)\./);
-						if (mfirst) { 
-							company = mfirst[1]; 
-						} else {	
-							console.error('no company for host ', host); return r; 
-						}
-					} 
-					r[company] = _.union(r[company] || [], host_pis);
-					return r;
-				}, {});
-			},
-			makeCategories:(appCompany, details, c2pi) => {
-				'app-publisher': _.pickBy(c2pi, (pis, company) => 
-					utils.matchCompany(appCompany, company)),
-				'app-functionality': _.pickBy($scope.company2pi, (pis, company) => 
-					!utils.isType(details, company, 'ignore') && 
-					!utils.isType(details, company, 'platform') && 
-					utils.is3rdPartyType(details, company'app')),
-				'marketing': _.pickBy($scope.company2pi, (pis, company) => 
-					!utils.isType(details, company, 'ignore') && 
-					!utils.isType(details, company, 'platform') && 
-					utils.is3rdPartyType(details, company'marketing')),
-				'usage tracking': _.pickBy($scope.company2pi, (pis, company) => 
-					!utils.isType(details, company, 'ignore') && 
-					!utils.isType(details, company, 'platform') && 
-					utils.is3rdPartyType(details, company 'usage')),
-				'payments':_.pickBy($scope.company2pi, (pis, company) => 
-					!utils.isType(details, company, 'ignore') && 
-					!utils.isType(details, company, 'platform') && 
-					utils.is3rdPartyType(details, company 'payments')),
-				'security':_.pickBy($scope.company2pi, (pis, company) => 
-					!utils.isType(details, company, 'ignore') && 
-					!utils.isType(details, company, 'platform') && 
-					utils.is3rdPartyType(details, company 'security')),
-				'other': _.pickBy($scope.company2pi, (pis, company) => 
-					!utils.matchCompany(appCompany, company) &&							
-					!utils.isType(details, company, 'ignore') && 							
-					!utils.isType(details, company, 'app') && 							
-					!utils.isType(details, company,'marketing') &&
-					!utils.isType(details, company, 'platform') && 							
-					!utils.isType(details, company, 'usage') &&
-					!utils.isType(details, company, 'payments') &&
-					!utils.isType(details, company, 'security'))
-			}
-		};
-		return utils;
-	}).config(function ($stateProvider, $urlRouterProvider) {
+	.config(function ($stateProvider, $urlRouterProvider) {
 		$stateProvider.state('dci.table', {
 			url: '/table',
 			templateUrl: 'tmpl/table.html',
@@ -91,35 +12,30 @@ angular.module('dci')
 				data: ($http) => $http.get('../mitm_out/data_all.json').then((x) => x.data)
 			},
 			controller:function($scope, pitypes, hosts, details, data, utils, $stateParams) {
+				console.info('hello table');
 				data = $scope.data = data.filter((x) => x.app === $stateParams.app);
-
 				var app = $scope.app = $stateParams.app,
 					appcompany = $scope.appcompany = data[0].company,
-					id2names = $scope.id2names = utils.makeId2Names(details),
-					isType = $scope.isType = (id,type) => utils.isType(details,id,type),
-					matchCompany = (x) => utils.matchCompany(appcompany, x)
 					getName = $scope.getName = (id) => details[id] && details[id].company || id,
-					is3rdPartyType = $scope.is3rdPartyType = (id,type) => utils.is3rdPartyType(details,id,type),
-					c2pi = utils.makeCompany2pi(data, hosts),
-					cat2c2pi = utils.makeCategories(c2pi);
-
+					c2pi = $scope.c2pi = utils.makeCompany2pi(app, data, hosts, pitypes, 0),
+					cat2c2pi = $scope.cat2c2pi = utils.makeCategories(appcompany, details, c2pi),
 					recompute = () => {					
 						// each of the boxes
-						$scope.company2pi = c2pi;
-						$scope.categories = $scope.categories = 
+						$scope.pilabels = utils.pilabels;
+						$scope.pitypes = _(c2pi).values().flatten().uniq().sort((x) => utils.pilabels[x]).value();
+						$scope.companies = _(c2pi).keys().uniq().sort((x) => getName(x)).value();
 					};
 
 				if (!appcompany) { $scope.error = 'Captured data for ' + app + ' is in old data format without company field'; }
 				if (!hosts[$scope.app]) { $scope.error = 'No hosts known for app'; }
-
 				$scope.size = (l) => _.keys(l).length;
-				$scope.threshold = 0;
-				$scope.$watch('threshold', () => { if ($scope.threshold!==undefined) { recompute(); }});
+				recompute();
 
 				$scope.hosts = hosts;
 				$scope.data = data;
-				$scope.pitypes = pitypes;
-				$scope.details = details;
+				// $scope.pitypes = pitypes;
+				// $scope.details = details;
 				window._s = $scope;
 			}
 		});
+	});
