9ccc9c688ead30d70f64075299f7e7ff44edafcd
electronic Max <max@hip.cat>
2016-08-24 01:43:12 +0100

    perms cleaned up

--- prototypes/js/experiment.js
+++ prototypes/js/experiment.js
@@ -124,7 +124,7 @@ angular.module('dci')
 				console.log('config');
 				var apps = $scope.apps,
 					range = $scope.range,
-					ifaces = $scope.ifaces = ['table','sankey','box', 'tablepl'];
+					ifaces = $scope.ifaces = ['table','sankey','box', 'tablepl', 'permission', 'permission with purpose'];
 				$scope.genID = utils.guid;
 				$scope.participantid = 'part-'+utils.guid(4);
 				$scope.runid = 'run-'+utils.guid(4);
