3e7eb9bc9f74ccfef0f52a2933128162df9b1f85
electronic Max <electronic@gmail.com>
2016-09-13 17:19:05 +0100

    unbundle

--- mitm/tag-unbundle.js
+++ mitm/tag-unbundle.js
@@ -77,8 +77,28 @@ var loadCSV = (fname) => {
 			rej(err);
 		});
 	});	
-};
+}, consolidate = (fname, raters, choose_tags) => {
+	// console.info('got cols');
+	var rows = loadCSV(fname),
+		rater_a = raters[0], rater_b = raters[1],
+		tags = choose_tags || _.uniq(_.flatten(rows.map((r) => _.flatten(raters.map((c) => s(r[c])))))).sort(),
+		header = ['id'].concat(tags.reduce((cat, T) => cat.concat([T]), [])),
+		rowos = [header].concat(
+			rows.map((r) => {
+				return [r.id].concat(tags.reduce((cat, T) => {
+					return cat.concat( _.flatten(raters.map((rater) => s(r[rater]))).indexOf(T) >= 0 ? 1 : 0 );
+				}, []));
+			}));
 
+	// console.log('tags ', rowos);
+	return new Promise((acc, rej) => {
+		csvstr(rowos, (err, output) => {
+			if (!err) { return acc(output); }
+			console.error("Error ", err);
+			rej(err);
+		});
+	});	
+};
 
 if (require.main === module) { 
 	// node command fileout pers1 pers2 krips/separate
@@ -88,6 +108,15 @@ if (require.main === module) {
 			// write it !
 			console.info("Writing output ", '../mitm_out/tag-krips.csv', data.length);
 			fs.writeFileSync('../mitm_out/tag-krips.csv', data);
+			console.log('done');
+		});
+		return;
+	}
+	if (process.argv[5] === 'consolidate') {
+		console.info('consolidate!');
+		consolidate(process.argv[2], process.argv.slice(3,5)).then((data) => {
+			console.info("Writing output ", '../mitm_out/tag-c.csv', data.length);
+			fs.writeFileSync('../mitm_out/tag-c.csv', data);
 			console.log('done');
 		});
 		return;
