955dd513723058e258845d4d1a4dd66bfb51eebb
electronic Max <max@hip.cat>
2016-10-27 23:42:14 +0100

    app vs web pairing

---
+++ static/app-vs-web.js
@@ -0,0 +1,84 @@
+
+
+// this module 
+// input:
+//   app-pairs.csv - app apk vs web pairing
+//   trie-5k.json - breakdown of apk to lib 
+//   lib-to-company - mapping from library to company
+//   web-to-company - mapping from website to company
+// 	 company-metadata.csv (parent relationships and core metadata including juristidction)
+// 
+// outputs: csv file: 
+//   app apk, web site, app trackers, web trackers, # of app trackers, # of web trackers, intersection, # of app companies, # of web companies, intersection
+
+var utils = require('./utils'),
+	fs = require('fs'),
+	promise = require('bluebird'),
+	_ = require('lodash'),
+	chop_2ld = (name) => {
+		var match = name.match(/^[^\.]*\.[^\.]*/);
+		if (match) { 
+			return match[0];
+		}
+		return name;
+	},
+	canonicalise_2ld = (url) => {
+		url = url.toLowerCase().trim();
+		if (url.indexOf('http') >= 0) { url = url.slice(url.indexOf('//')+2); }
+		if (url.indexOf('/') >= 0) { url = url.slice(0,url.indexOf('/')); }
+		return utils.shorten_2ld(url);
+	};
+
+if (require.main === module) { 
+	if (process.argv.length < 7) { 
+		console.log(`input:
+			app-pairs.csv - app apk vs web pairing
+			trie-5k.json - breakdown of apk to lib 
+			lib-to-company.csv - mapping from library to company
+			trackers.csv - list of trackers per website
+			web-to-company.csv - mapping from website to company
+			company-metadata.csv - path to company metadata
+		out: 
+			app-overlap.csv `);
+	} else {
+
+		var app_pairs = utils.loadCSV(process.argv[2]).map((pair) => { pair.url = canonicalise_2ld(pair["web site"]); return pair; }),
+			app_libs = JSON.parse(fs.readFileSync(process.argv[3])),
+			lib_company = utils.loadCSV(process.argv[4]).filter((x) => x.type.indexOf('ads') >= 0).reduce((d, lib) => { d[lib.package.toLowerCase()] = lib.company.toLowerCase().trim(); return d; }, {}),
+			web_company = utils.loadCSV(process.argv[6]).filter((x) => x.type.indexOf('advert') >= 0).reduce((d, tr) => { d[tr.host.toLowerCase()] = tr.company.toLowerCase().trim(); return d; }, {}),			
+			web_trackers = utils.loadCSV(process.argv[5], ['company','jurisdiction','url']).reduce((d, set) => {
+				var url = canonicalise_2ld(set.url);
+				d.company = d.company.toLowerCase();
+				if (d.company && web_company[d.company] && web_company[d.company]) { 
+					// include only ads
+					d[url] = (d[url] || []).concat(d.company);
+				}
+				return d;
+			}),
+			company_metadata = utils.loadCSV(process.argv[7]).reduce((d,cinfo) => {	d[cinfo.id] = cinfo; return d;	});
+
+		var rows = app_pairs.map((pair) => {
+			//  ------------------------->
+			var applibs = app_libs[pair.apk].map((lib) => lib_company[lib]).filter((x) => x),
+				webtks = web_company[pair.url];
+
+			if (!applibs) { 
+				console.error('no app libs in trie for ', pair.apk);
+				return;
+			}
+			if (!webtks) {
+				console.error('no entry for ', pair.url, 'skipping');
+				return;
+			}
+
+			var intersection = _.intersectin(applibs, webtks);
+
+			console.info(` ${pair.apk}(${applibs.length}) vs ${pair.url}(${webtks.length}) - intersection: ${intersection} `);
+			return [pair.apk, pair.url, applibs, applibs.length, webtks, webtks.length, intersection, intersection.length];
+			// return [pair.apk, pair.url, applibs.length, webtks.length, intersection.length];
+		}).filter((x) => x);
+
+		console.info(`writing csv to ${process.argv[8]}, ${rows.length}`);
+		utils.writeCSV(process.argv[8]);
+	}
+}
--- static/utils.js
+++ static/utils.js
@@ -3,11 +3,11 @@ var fs = require('fs'),
 	parse = require('csv-parse/lib/sync'),
 	csvstr = require('csv-stringify'),
 	_ = require('lodash'),	
-    loadCSV = (fname) => {
+    loadCSV = (fname, columns) => {
 		var text = 	fs.readFileSync(fname).toString();
 		console.error("Parsing file ", fname, "(", text.length, ")");
 		var data = parse(text, {max_limit_on_data_read:9999999999});
-		var headers = data[0];
+		var headers = columns || data[0];
 		data = data.slice(1);
 		data = data.map((x) => _.zipObject(headers,x));
 		return data;
@@ -23,7 +23,36 @@ var fs = require('fs'),
 			console.error('error ', err);
 			rej(err);
 		});
-	});
+	}),
+	getSLDs = () => {
+		var ccsld = fs.readFileSync('../mitm/curated/ccsld.txt').toString();
+		return ccsld.split('\n').filter((x) => (x && x.trim().length > 0 && x.indexOf('.') >= 0 && x.indexOf('//') < 0 &&  x.indexOf('!') < 0 && x.indexOf('*') < 0));
+	}, 	
+	ipv4re = /((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/,
+	shorten_2ld = (host) => {
+		host = host.trim().toLowerCase();
+		// raw ip addresses don't get shortened
+		if (ipv4re.test(host)) { 
+			console.log('detected an ipv4 address, skipping shortening', host);
+			return host;	
+		}
+		var match = host.match(/([^\.]*)\.([^\.]*)$/);
+		if (match) { 
+			var short = match[0];
+			if (exports.ccslds.indexOf(short) >= 0) { 
+				var onemore = host.slice(0,host.length - short.length - 1).match(/([^\.]*)$/);
+				if (onemore) { 
+					return [onemore[0], short].join('.');
+				} else {
+					return host; 
+				}
+			}
+			return short;
+		}
+		return host;
+	};
 
+
+exports.ccslds = getSLDs();
 exports.loadCSV = loadCSV;
 exports.saveCSV = write_csv;
