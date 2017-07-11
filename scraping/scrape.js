7c616ebfb1112dcd8aa6fe965a918759edabddfb
deanott <dean.p.ottewell@gmail.com>
2017-07-11 07:49:10 +0100

    one file download apk based on googleplaystore node module + python downloader spawner

---
+++ scraping/scrape.js
@@ -0,0 +1,45 @@
+
+/* 
+
+Background ndoe script scraping apks from playstore.
+Categorising into folders on genre. 
+Does a apk download step by step and just spawning a child 
+python process. That downloader was cool so hey, why not use it!
+
+
+https://www.npmjs.com/package/google-play-scraper
+
+NOTE: 120 is max number of apps to receive at once through list method...
+
+TODO: to avoid the situation of askign for a captcha use a throttle keyword
+, all methods now support a throttle property, which defines an upper bound to the amount of requests that will be attempted per second. Once that limit is reached, further requests will be held until the second passes.
+
+TODO: we can a call a update to a folder through python periodically... does node.js forever support that kind of shenanigans...
+
+*/ 
+var gplay = require('google-play-scraper'); //google play store query scrapper. 
+var PythonShell = require('python-shell'); //Shell based python spawn - cleaner + get errs
+
+
+
+//Iterate over all the gplay store files... 
+//Can do this through a method of og g-play-scraper via category. 
+//Could do it through searching for title a* ... there might be more than MAX query a* though
+
+
+//Comms with python download
+
+var PythonShell = require('python-shell');
+
+var options = {
+  scriptPath: '../../scrapeTools/gplaycli/gplaycli',
+  args: ["-d", "com.dxco.pandavszombies"]
+};
+
+//Pretty nice tool can just use this and then organise apps by section...
+PythonShell.run('gplaycli.py', options, function (err, results) {
+  if (err) throw err;
+  // results is an array consisting of messages collected during execution
+  console.log('results: %j', results);
+});
+
