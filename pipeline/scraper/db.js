3cf263d06074efb58327e0d8dbda0a18bf78a330
Sauyon Lee <sauyonl@sauyon.com>
2017-07-18 16:12:43 +0100

    remove semicolons from db

--- pipeline/scraper/db.js
+++ pipeline/scraper/db.js
@@ -29,14 +29,14 @@ function connect() {
 };
 
 async function insertDev(dev) {
-	var res = await pool.query("SELECT id FROM developers WHERE $1 = ANY(email);", [dev.email]);
+	var res = await pool.query("SELECT id FROM developers WHERE $1 = ANY(email)", [dev.email]);
 	if (res.length > 0) {
 		return res.rows[0].id;
 	}
 
 	// maybe dev id needs to be URL encoded?
 	let store_site = 'https://play.google.com/store/apps/developer?id='+dev.id;
-	res = await query("INSERT INTO developers(email,name,store_site,site) VALUES ($1, $2, $3, $4) RETURNING id;",
+	res = await query("INSERT INTO developers(email,name,store_site,site) VALUES ($1, $2, $3, $4) RETURNING id",
 	                  [[dev.email], dev.name, store_site, dev.site]);
 	return res.rows[0].id;
 }
@@ -60,7 +60,7 @@ module.exports = {
 			vers = res.rows[0].versions;
 			// app exists in database, check if version does as well
 			var res1 = await query(
-				"SELECT id FROM app_versions WHERE app = $1 AND store = $2 AND region = $3 AND version = $4;",
+				"SELECT id FROM app_versions WHERE app = $1 AND store = $2 AND region = $3 AND version = $4",
 				[ app.appId, 'play', region, app.version ]);
 
 			if (res1.length > 0) {
@@ -73,7 +73,7 @@ module.exports = {
 		var client = await connect();
 		console.log("Connected");
 
-		await client.query("BEGIN;"); // maybe this should be inside the try?
+		await client.query("BEGIN"); // maybe this should be inside the try?
 		try {
 			if (!verExists) {
 				if (!appExists) {
@@ -82,18 +82,18 @@ module.exports = {
 				}
 
 				let res = await client.query(
-					"INSERT INTO app_versions(app, store, region, version) VALUES ($1, $2, $3, $4) RETURNING id;",
+					"INSERT INTO app_versions(app, store, region, version) VALUES ($1, $2, $3, $4) RETURNING id",
 					[ app.appId, 'play', region, app.version ]
 				);
 				verId = res.rows[0].id;
 
-				await client.query("UPDATE apps SET versions=versions || $1 WHERE id = $2;",
+				await client.query("UPDATE apps SET versions=versions || $1 WHERE id = $2",
 				                   [ [verId], app.appId ]);
 
 			}
 
 			await client.query(
-				"INSERT INTO playstore_apps VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21);",
+				"INSERT INTO playstore_apps VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)",
 				[
 					verId,
 					app.title,
@@ -117,9 +117,9 @@ module.exports = {
 					app.recentChanges,
 					new Date().toDateString()
 				]);
-			await client.query("COMMIT;");
+			await client.query("COMMIT");
 		} catch (e) {
-			await client.query("ROLLBACK;");
+			await client.query("ROLLBACK");
 			throw e;
 		} finally {
 			client.release();
