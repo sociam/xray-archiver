a3242511d0d0a3558791d298c2a29e646f83d25f
Adam Slack <adamslack@outlook.com>
2017-08-03 13:31:39 +0100

    Added Comments to each exported method and var.
    The changes satisfy all comments made by the golint

--- pipeline/util/config.go
+++ pipeline/util/config.go
@@ -9,6 +9,7 @@ import (
 	"path"
 )
 
+// DBCfg Struct for the Database Config File information
 type DBCfg struct {
 	Database string `json:"database"`
 	User     string `json:"-"`
@@ -17,19 +18,27 @@ type DBCfg struct {
 	Port     int    `json:"port"`
 }
 
+// DBCreds Struct for the Database Credentials
 type DBCreds struct {
 	User     string `json:"user"`
 	Password string `json:"password"`
 }
 
+// AnalyzerCfg Represents the Credentials used to connect to the DB
+// as the Analyser
 type AnalyzerCfg struct {
 	DB DBCreds `json:"db"`
 }
 
+// APIServCfg Represents the Credentials used to connect to the DB
+// as the API Server
 type APIServCfg struct {
 	DB DBCreds `json:"db"`
 }
 
+// Config Struct for the Xray Config information relating to Dir and file
+// locations. As well as holding DB, Analyser and APIServ Config
+// information.
 type Config struct {
 	EDIHostname string      `json:"edihost"`
 	DataDir     string      `json:"datadir"`
@@ -40,13 +49,20 @@ type Config struct {
 	DB          DBCfg       `json:"db"`
 }
 
+// Cfg is an instance of Config that will contain DB settings
+// as well as Dir/file locations and information.
 var Cfg Config
 
+// Enum used when loading the config in LoadCfg
 const (
 	Analyzer = iota
 	APIServ
 )
 
+// LoadCfg Opens a config file and creates a series of objects
+// using the information located in the file. It constructs a
+// Config, populating information for the Analyser Config,
+// API Server Config and the DB config.
 func LoadCfg(cfgFile string, requester int) error {
 	file, err := os.Open(cfgFile)
 	bytes, err := ioutil.ReadAll(file)
