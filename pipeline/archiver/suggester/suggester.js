405656aa3c3c9168e752c451b6c2391c9f166e88
Adam Slack <adamslack@outlook.com>
2017-08-09 12:26:42 +0100

    Started to fetch playstore links from suggested alt app pages

--- pipeline/archiver/suggester/suggester.js
+++ pipeline/archiver/suggester/suggester.js
@@ -18,10 +18,11 @@ var db = new Database('suggester');
  * The urls collected here will point towards another alternativeTo Page
  * that will have a store link (hopefully)
  */
-function fetchAlternatives(urlString) {
+function scrapePageForAlts(URLString) {
 
+    //TODO: Check that page is an app page.
     // Request the page from the Site.
-    request(urlString, (err, res, html) => {
+    request(URLString, (err, res, html) => {
 
         // if there wasn't an err with the request.
         if (err) {
@@ -52,13 +53,10 @@ function fetchAlternatives(urlString) {
         }
 
         // for each ID find the Title and URL to AltTo page.
-        _.forEach(altIDs, (altID) => {
+        altApps = _.map(altIDs, (altID) => {
             //logger.debug(altID);
             var aTag = $('#' + altID).find('h3').find('a').first();
-            altApps.push({
-                'title': aTag.text(),
-                'AltToURL': aTag.attr('href')
-            });
+            return {'title': aTag.text(), 'AltToURL': aTag.attr('href')};
         });
         logger.debug(altApps);
 
@@ -67,7 +65,33 @@ function fetchAlternatives(urlString) {
             return 'No Alternative App links/Titles could be found. Bailing!';
         }
 
+        _.forEach(altApps, (altApp) => {
+            var url = 'http://alternativeto.net' + altApp.AltToURL;
+            scrapeAltAppPage(url);
+        });
+
     });
 }
 
-fetchAlternatives('http://alternativeto.net/browse/search/?license=free&platform=android&q=facebook');
+/**
+ * Give a URL to a specific suggested Alternative App page's URL
+ * this function will scrape the Playstore URL or failing that
+ * the Developers Website page. Failing that it will just return
+ * Alternative To URL for the page.
+ */
+function scrapeAltAppPage(URLString) {
+    request(URLString, (err, res, html) => {
+
+        // if there wasn't an err with the request.
+        if (err) {
+            logger.err('Failed to fetch HTML: ' + err);
+            return err;
+        }
+
+        // Initialising Variables and Loading HTML into Cheerio
+        var $ = cheerio.load(html);
+        logger.debug(URLString);
+    });
+}
+
+scrapePageForAlts('http://alternativeto.net/browse/search/?license=free&platform=android&q=facebook');
