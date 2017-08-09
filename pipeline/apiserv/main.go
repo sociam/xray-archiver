9ccd6a67f7271ab6374ca94f57d9d1cea33ff85a
Adam Slack <adamslack@outlook.com>
2017-08-09 18:59:14 +0100

    Allow cross origin again (#38)
    Adding CORS again

--- pipeline/apiserv/main.go
+++ pipeline/apiserv/main.go
@@ -122,6 +122,11 @@ func parseOffset(num string) (val string, oops string, err error) {
 
 func appsEndpoint(w http.ResponseWriter, r *http.Request) {
 	mime := r.Header.Get("Accept")
+
+	// DONT DELETE DEAN.
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	// DEAN... DONT DELETE.
+
 	//Check input
 	if r.Method == "POST" || r.Method == "GET" {
 		if _, ok := supportedMimes[mime]; !ok {
@@ -237,6 +242,9 @@ func appsEndpoint(w http.ResponseWriter, r *http.Request) {
 // altAppsEndpoint allows for external entities to query for alternative apps based on app ID.
 func altAppsEndpoint(w http.ResponseWriter, r *http.Request) {
 	mime := r.Header.Get("Accept")
+	// DEAN DON'T DELETE THIS EITHER
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	// DEAN
 	if r.Method == "POST" || r.Method == "GET" {
 		if _, ok := supportedMimes[mime]; !ok {
 			writeErr(w, mime, http.StatusNotAcceptable, "not_acceptable", "Yo Dawg, we deal with json only son.")
