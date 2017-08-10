01c138c43badd280c1e9864e4227d9925c449d39
Sauyon Lee <s@uyon.co>
2017-08-10 13:44:05 +0100

    untabify!

--- pipeline/scripts/install.sh
+++ pipeline/scripts/install.sh
@@ -5,7 +5,7 @@ set -u
 set -f
 
 usage() {
-	echo <<EOF
+  echo <<EOF
 Usage: $0 [OPTIONS]
 
 options:
@@ -18,55 +18,55 @@ EOF
 
 PREFIX="/usr/local"
 while [[ $# > 2 ]]; do
-	case "$1" in
-		-h|--help) usage; exit 0;;
-		-d|--data-dir) DATA_DIR="$2"; shift ;;
-		-p|--prefix) PREFIX="$2"; shift ;;
-		--) shift; break;;
-		*)	usage; exit 64 ;;
-	esac
-	shift
+  case "$1" in
+    -h|--help) usage; exit 0;;
+    -d|--data-dir) DATA_DIR="$2"; shift ;;
+    -p|--prefix) PREFIX="$2"; shift ;;
+    --) shift; break;;
+    *)  usage; exit 64 ;;
+  esac
+  shift
 done
 
 : ${DATA_DIR:="$PREFIX/var/xray"}
 
 if [[ $UID -ne 0 ]]; then
-	echo "Please run this script as root!"
-	exit 77
+  echo "Please run this script as root!"
+  exit 77
 fi
 
 if [[ ! -d analyzer ]] || [[ ! -d archiver ]] || [[ ! -d db ]]; then
-	echo "analyzer, archiver and db directories not found! Please run this script from the pipeline directory of the repository!"
-	exit 64
+  echo "analyzer, archiver and db directories not found! Please run this script from the pipeline directory of the repository!"
+  exit 64
 fi
 
 if ! type npm &> /dev/null; then
-	echo "npm is not installed!"
-	exit 4
+  echo "npm is not installed!"
+  exit 4
 fi
 
 if [[ ! -x analyzer/analyzer ]]; then
-	if ! type go &> /dev/null; then
-		echo "analyzer binary doesn't exist and go is not installed!"
-		exit 5
-	fi
-	cd analyzer
-	go build
-	cd -
+  if ! type go &> /dev/null; then
+    echo "analyzer binary doesn't exist and go is not installed!"
+    exit 5
+  fi
+  cd analyzer
+  go build
+  cd -
 fi
 
 if [[ ! -x apiserv/apiserv ]]; then
-	if ! type go &> /dev/null; then
-		echo "apiserv binary doesn't exist and go is not installed!"
-		exit 5
-	fi
-	cd apiserv
-	go build
-	cd -
+  if ! type go &> /dev/null; then
+    echo "apiserv binary doesn't exist and go is not installed!"
+    exit 5
+  fi
+  cd apiserv
+  go build
+  cd -
 fi
 
 if ! id -u xray &> /dev/null; then
-	useradd xray -r -d "$DATA_DIR"
+  useradd xray -r -d "$DATA_DIR"
 fi
 
 install -o xray -Ddm775 "$DATA_DIR/apk_archive"
