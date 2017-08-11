8dc3357839ba4afbfd34c0a9bcdc7cffc997272d
Dean P. Ottewell <dean.p.ottewell@gmail.com>
2017-08-11 14:14:09 +0100

    Go logger setup  (#47)
    Logger supports systemd and changed most calls of go files to now use the logger

--- pipeline/db/db.go
+++ pipeline/db/db.go
@@ -396,14 +396,14 @@ func GetApps(num, start int) ([]App, error) {
 			&app.ID,
 			pq.Array(&app.Vers))
 		if err != nil {
-			fmt.Println("Database err:", err)
+			util.Log.Err("Database err:", err)
 		} else {
 			ret = append(ret, app)
 		}
 	}
 
 	if rows.Err() != sql.ErrNoRows && rows.Err() != nil {
-		fmt.Println("Databse err", rows.Err())
+		util.Log.Err("Databse err", rows.Err())
 		return []App{}, rows.Err()
 	}
 
@@ -431,7 +431,7 @@ func GetLatestApps(num, start int) ([]App, error) {
 	}
 
 	if rows.Err() != sql.ErrNoRows && rows.Err() != nil {
-		fmt.Println("Databse err", rows.Err())
+		util.Log.Err("Databse err", rows.Err())
 		return []App{}, rows.Err()
 	}
 
@@ -456,7 +456,7 @@ func GetAltApps(appID string) ([]AltApp, error) {
 	}
 
 	if rows.Err() != sql.ErrNoRows && rows.Err() != nil {
-		fmt.Println("Database err", rows.Err())
+		util.Log.Err("Database err", rows.Err())
 	}
 
 	return result, nil
@@ -604,7 +604,7 @@ func QuickQuery(
 			pq.Array(&perms), //pq.Array(&appData.Perms),
 			pq.Array(&pkgs))  //pq.Array(&appData.Packages))
 		if err != nil {
-			fmt.Println("Database Query", err)
+			util.Log.Err("Database Query", err)
 		} else {
 
 			//info.FamilyGenre = famGenre.Valid ? famGenre.String : ""
@@ -636,7 +636,7 @@ func QuickQuery(
 	}
 
 	if rows.Err() != sql.ErrNoRows && rows.Err() != nil {
-		fmt.Println("Database err", rows.Err())
+		util.Log.Err("Database err", rows.Err())
 	}
 
 	return result, nil
@@ -663,7 +663,7 @@ func GetAppsToAnalyze() ([]AppVersion, error) {
 		cur.Icon = icon.String
 
 		if err != nil {
-			fmt.Println(err)
+			util.Log.Err("Getting apps to analyzed errors", err)
 		} else {
 			ret = append(ret, cur)
 		}
---
+++ pipeline/util/logger.go
@@ -0,0 +1,68 @@
+package util
+
+import (
+	"fmt"
+	"strings"
+)
+
+//Logger For Systemd logs
+var prefixes = []string{"<0>", "<1>", "<2>", "<3>", "<4>", "<5>", "<6>", "<7>"}
+
+// Debug levels corresponding to the syslog levels
+const (
+	EMERG int = iota
+	ALERT
+	CRIT
+	ERR
+	WARNING
+	NOTICE
+	INFO
+	DEBUG
+)
+
+// func log(level DebugLevel, args ...string) {
+// 	fmt.Println(prefixes[level], args)
+// }
+
+type logger struct{}
+
+// Log is the namespace for the logger functions
+var Log = logger{}
+
+func (l logger) Log(level int, str string, args ...interface{}) {
+	fmt.Print(prefixes[level] +
+		strings.Replace(fmt.Sprintf(str, args...),
+			"\n", "\n"+prefixes[level], -1))
+}
+
+func (l logger) Emerg(str string, args ...interface{}) {
+	l.Log(EMERG, str, args...)
+}
+
+func (l logger) Alert(str string, args ...interface{}) {
+	l.Log(ALERT, str, args...)
+}
+
+func (l logger) Crit(str string, args ...interface{}) {
+	l.Log(CRIT, str, args...)
+}
+
+func (l logger) Err(str string, args ...interface{}) {
+	l.Log(ERR, str, args...)
+}
+
+func (l logger) Warning(str string, args ...interface{}) {
+	l.Log(WARNING, str, args...)
+}
+
+func (l logger) Notice(str string, args ...interface{}) {
+	l.Log(NOTICE, str, args...)
+}
+
+func (l logger) Info(str string, args ...interface{}) {
+	l.Log(INFO, str, args...)
+}
+
+func (l logger) Debug(str string, args ...interface{}) {
+	l.Log(DEBUG, str, args...)
+}
