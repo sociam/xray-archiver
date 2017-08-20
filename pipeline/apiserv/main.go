42c84cbef9d5b416d3ef793b44cca7835fc8b4a9
Dean P. Ottewell <dean.p.ottewell@gmail.com>
2017-08-20 20:47:01 +0100

    Linter fixes`

--- pipeline/apiserv/main.go
+++ pipeline/apiserv/main.go
@@ -395,7 +395,7 @@ func fetchHosts(w http.ResponseWriter, r *http.Request) {
 		hostToGeoip := map[string][]util.GeoIPInfo{}
 
 		for i := range hosts {
-			util.Log.Debug("Getting host geo ip: %s\n", hosts[i] )
+			util.Log.Debug("Getting host geo ip: %s\n", hosts[i])
 			var geoip []util.GeoIPInfo
 
 			geoip, err = util.GetHostGeoIP(hosts[i])
@@ -415,7 +415,7 @@ func fetchHosts(w http.ResponseWriter, r *http.Request) {
 }
 
 var cfgFile = flag.String("cfg", "/etc/xray/config.json", "config file location")
-var port = flag.Uint("port", 6666, "Port to serve on.")
+var port = flag.Uint("port", 8118, "Port to serve on.")
 
 func init() {
 	util.LoadCfg(*cfgFile, util.APIServ)
