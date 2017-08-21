e951d56169498545a95fba8b206f427343164b1b
Dean P. Ottewell <dean.p.ottewell@gmail.com>
2017-08-22 00:14:26 +0100

    Fixed port change

--- pipeline/apiserv/main.go
+++ pipeline/apiserv/main.go
@@ -341,7 +341,7 @@ func altAppsEndpoint(w http.ResponseWriter, r *http.Request) {
 }
 
 var cfgFile = flag.String("cfg", "/etc/xray/config.json", "config file location")
-var port = flag.Uint("port", 6666, "Port to serve on.")
+var port = flag.Uint("port", 8118, "Port to serve on.")
 
 func init() {
 	util.LoadCfg(*cfgFile, util.APIServ)
