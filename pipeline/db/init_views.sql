6a9d3ddb171f47d84a9aa145e645060ca4e96810
Adam Slack <adamslack@outlook.com>
2017-09-06 16:00:55 +0100

    swapped from views to tables. who needs views eh? they're not very fast for what we want :L

--- pipeline/db/init_views.sql
+++ pipeline/db/init_views.sql
@@ -1,9 +1,13 @@
 begin;
+---------------------------------------------------------------------------------------------------
+-- SUMMARY STATISTICS VIEWS AND TABLES
+---------------------------------------------------------------------------------------------------
 
--- SUMMARY STATISTICS VIEWS
-
+---------------------------------------------------------------------------------------------------
 -- Average number of hosts for each genre
-create view genre_host_averages as
+---------------------------------------------------------------------------------------------------
+drop table if exists genre_host_averages
+create table genre_host_averages as
   select category, host_count, app_count, host_count/app_count::float as host_avg from (
     select p.genre as category, sum(array_length(v.hosts,1)
   ) as host_count, count(p.genre) as app_count
@@ -11,40 +15,56 @@ create view genre_host_averages as
     group by genre) as genre_freq;
 
 grant select on genre_host_averages to apiserv;
-
-
+---------------------------------------------------------------------------------------------------
+-- table of distinct hosts 
+---------------------------------------------------------------------------------------------------
+drop table if exists distinct_hosts;
 create table distinct_hosts as
   select distinct hosts from ( 
     select unnest(hosts) as hosts from app_hosts
   ) as unpack_hosts;
-
-create view distinct_app_hosts as
+---------------------------------------------------------------------------------------------------
+-- table of app-host pairs
+---------------------------------------------------------------------------------------------------
+drop table if exists distinct_app_hosts;
+create table distinct_app_hosts as
   select distinct id, hosts from ( 
     select id, unnest(hosts) as hosts from app_hosts
   ) as unpack_hosts;
 
 
-create view host_app_coverage as
+---------------------------------------------------------------------------------------------------
+-- table counting the number of apps that feature specific hosts.
+---------------------------------------------------------------------------------------------------
 
+drop table if exists host_app_coverage;
+create table host_app_coverage as
+  select hosts, count(*) from distinct_app_hosts
+    group by hosts;
 
+grant select on host domains to apiserv
+---------------------------------------------------------------------------------------------------
 -- Table of all possible Host names and a heuristic regex for the domain of the host.
--- create view host_domains as
---   select hosts,
---          substring(hosts from '(([^\.]*)\.([^\.]*)$)') as domain,
---          substring(hosts from '(([^\.]*)\.([^\.]*)\.([^\.]*)$)') as domain_plus from ( 
---     select distinct hosts from ( 
---       select unnest(hosts) as hosts from app_hosts
---     ) as unpack_hosts
---   ) as distinct_hosts;
+---------------------------------------------------------------------------------------------------
+drop table if exists host_domains;
+create table host_domains as
+  select hosts,
+    substring(hosts from '(([^\.]*)\.([^\.]*)$)') as domain,
+    substring(hosts from '(([^\.]*)\.([^\.]*)\.([^\.]*)$)') as domain_plus from distinct_hosts;
 
 grant select on host_domains to apiserv
-
+---------------------------------------------------------------------------------------------------
 -- Table of Host, heuristic based domain and company for that domain.
--- create view host_domain_companies as 
---   select distinct d.hosts, d.domain, d.domain_plus, coalesce(c.company, 'unknown') as company
---     from host_domains d, company_domains c
---       where d.domain = c.domain 
---          or d.domain_plus = c.domain;
+---------------------------------------------------------------------------------------------------
+drop table if exists host_domain_companies;
+create table host_domain_companies as 
+  select distinct d.hosts, d.domain, d.domain_plus, coalesce(c.company, 'unknown') as company
+    from host_domains d left outer join company_domains c
+      on( d.domain = c.domain 
+         or d.domain_plus = c.domain
+         or lower(d.hosts) ilike '%' || lower(c.domain) || '%');
+
+grant select on host_domain_companies to apiserv
 
 -- All Hosts
 -- create view all_hosts as
