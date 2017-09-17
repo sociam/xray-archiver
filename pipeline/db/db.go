5e35e6eb08cbef68f378d10a900eee6dff3c4915
Dean P. Ottewell <dean.p.ottewell@gmail.com>
2017-09-17 10:54:18 +0100

    Db changed to call new all_apps_data

--- pipeline/db/db.go
+++ pipeline/db/db.go
@@ -709,19 +709,28 @@ func QueryAll(
 ) ([]AppVersion, error) {
 
 	var querystr string
-	querystr = "SELECT " +
-		"a.id, a.title, a.summary, a.description, a.store_url, a.price, a.free, a.rating, " +
-		"a.num_reviews, a.genre, a.family_genre, a.min_installs, a.max_installs, a.updated, " +
-		"a.android_ver, a.content_rating, a.recent_changes, v.app, v.store, v.region, " +
-		"v.version, v.icon, v.analyzed, d.email, d.name, d.store_site, d.site, h.hosts, p.permissions, " +
-		"pkg.packages " +
-		"FROM " + appStoreTable[appStore] + " a " +
-		"FULL OUTER JOIN app_versions v ON (a.id = v.id) " +
-		"FULL OUTER JOIN developers d ON (a.developer = d.id) " +
-		"FULL OUTER JOIN app_hosts h ON (a.id = h.id) " +
-		"FULL OUTER JOIN app_perms p ON (a.id = p.id) " +
-		"FULL OUTER JOIN app_packages pkg  ON (a.id = pkg.id) " +
-		"WHERE "
+	// querystr = "SELECT " +
+	// 	"a.id, a.title, a.summary, a.description, a.store_url, a.price, a.free, a.rating, " +
+	// 	"a.num_reviews, a.genre, a.family_genre, a.min_installs, a.max_installs, a.updated, " +
+	// 	"a.android_ver, a.content_rating, a.recent_changes, v.app, v.store, v.region, " +
+	// 	"v.version, v.icon, v.analyzed, d.email, d.name, d.store_site, d.site, h.hosts, p.permissions, " +
+	// 	"pkg.packages " +
+	// 	"FROM " + appStoreTable[appStore] + " a " +
+	// 	"FULL OUTER JOIN app_versions v ON (a.id = v.id) " +
+	// 	"FULL OUTER JOIN developers d ON (a.developer = d.id) " +
+	// 	"FULL OUTER JOIN app_hosts h ON (a.id = h.id) " +
+	// 	"FULL OUTER JOIN app_perms p ON (a.id = p.id) " +
+	// 	"FULL OUTER JOIN app_packages pkg  ON (a.id = pkg.id) " +
+
+	querystr = "SELECT" +
+		"WHERE " +
+		"id, title, summary, description, store_url, price, free, rating, " +
+		"num_reviews, genre, family_genre, min_installs, max_installs, updated, " +
+		"android_ver, content_rating, recent_changes, app, store, region, " +
+		"version, icon, analyzed, email, name, store_site, site, hosts, permissions, " +
+		"packages " +
+		"FROM  all_apps_data" +
+		"WHERE"
 
 	var args []interface{}
 
