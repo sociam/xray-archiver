c7840dcbb639e59a5aa9c9812dbaac49fd5180c1
Adam Slack <adamslack@outlook.com>
2017-09-21 19:13:25 +0100

    should now have resolved mem leak error

--- pipeline/archiver/retriever/retriever.js
+++ pipeline/archiver/retriever/retriever.js
@@ -20,8 +20,8 @@ function insertAppData(appData) {
     }
 
     // push the app data to the DB
-    // return db.insertPlayApp(appData, region);
-    logger.debug(appData.appId);
+    return db.insertPlayApp(appData, region);
+    // logger.debug(appData.appId);
 }
 
 // TODO Add Permission list to app Data JSON
@@ -37,17 +37,44 @@ const fetchAppData = (searchTerm, numberOfApps, perSecond) => {
 
 let noErr = true;
 
-while (noErr) {
-    db.getStaleSearchTerms()
-        .then(
-            (searchTerms) => Promise.all(searchTerms.map((term) => fetchAppData(term.search_term, 120, 1))),
-            (err) => logger.err('ERROR READING SEARCH TERMS FROM DATABASE:', err))
-        .then(
-            (appDatas) => appDatas.map((appData) => logger.debug(appData)),
-            (err) => logger.err('ERROR FETCHING APP DATA FOR SEARCH TERMS:', err)
-        );
-    noErr = false;
-}
+(async() => {
+    while (noErr) {
+        await db.getStaleSearchTerms(1)
+            .then(
+                (searchTerms) => {
+                    const terms = searchTerms.map((term) => term.search_term);
+                    logger.debug('Marking Search terms as being used:', terms);
+                    terms.forEach((term) => db.updateLastSearchedDate(term)
+                        .then(
+                            () => logger.debug('Last Search date updated:', term),
+                            (err) => logger.err('ERROR SETTING SEARCH DATE FOR:', term, err)
+                        )
+                    );
+                    logger.debug('Fetching App data for searchterms:', terms);
+                    return Promise.all(terms.map((term) => fetchAppData(term, 120, 1)));
+                },
+                (err) => {
+                    noErr = false;
+                    logger.err('ERROR READING SEARCH TERMS FROM DATABASE:', err);
+                }
+            ).then(
+                (appDatas) => {
+                    const appData = appDatas.reduce((a, b) => a.concat(b), []);
+                    logger.debug('Filtering apps that already exist: ',
+                        appData.map((data) => data.appId));
+                    return appData.filter((appData) => db.doesAppExist(appData));
+                },
+                (err) => logger.err('ERROR FETCHING APP DATA FOR SEARCH TERMS:', err)
+            ).then(
+                (appDatas) => {
+                    logger.debug('App Data doesn\'t exist:', appDatas.map((data) => data.appId));
+                    return appDatas.forEach((data) => insertAppData(data)
+                        .catch((err) => logger.err('ERROR INSERTING APP INTO DATABASE:', err)));
+                },
+                (err) => logger.err('ERROR CHECKING EXISTANCE OF AN APP:', err)
+            );
+    }
+})();
 
 // // TODO: Move this to DB.
 // return Promise.all(_.map(appDatas, async(appData) => {
