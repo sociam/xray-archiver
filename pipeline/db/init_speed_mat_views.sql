275275f8362872fe0df9cc71e6402ba873727be5
Zoë <zoe.catchpole@stcatz.ox.ac.uk>
2017-09-26 11:52:22 +0100

    Added index generation and  cleaned up naming in init

--- pipeline/db/init_speed_mat_views.sql
+++ pipeline/db/init_speed_mat_views.sql
@@ -7,24 +7,29 @@ begin;
 -- GOTTA GO FAST 🦔
 ---------------------------------------------------------------------------------------------------
 
+--Setup materilized view namespace
 create schema matview
 
+--Ensure extension for index matching is setup
+create extension pg_trgm from unpackaged;
 
 -----
 -- Access speed for playstore apps data
 -----
 drop table if exists matview.apps_play_data cascade;
 
-create MATERIALIZED view matview.apps_play_data as SELECT 
+create materilized view matview.apps_play_data as select 
 a.id, a.title, a.summary, a.description, a.store_url, a.price, a.free, a.rating, 
 	a.num_reviews, a.genre, a.family_genre, a.min_installs, a.max_installs, a.updated, 
 	a.android_ver, a.content_rating, a.recent_changes, v.app, v.store, v.region,
 	v.version, v.icon, v.analyzed, d.email, d.name, d.store_site, d.site, h.hosts, p.permissions,
-	pkg.packages FROM playstore_apps a FULL OUTER JOIN app_versions v ON (a.id = v.id) 
-	FULL OUTER JOIN developers d ON (a.developer = d.id)  
-	FULL OUTER JOIN app_hosts h ON (a.id = h.id) 
-	FULL OUTER JOIN app_perms p ON (a.id = p.id) 
-	FULL OUTER JOIN app_packages pkg  ON (a.id = pkg.id)  ORDER BY max_installs;
+	pkg.packages from playstore_apps a FULL OUTER join app_versions v on (a.id = v.id) 
+	FULL OUTER join developers d on (a.developer = d.id)  
+	FULL OUTER join app_hosts h on (a.id = h.id) 
+	FULL OUTER join app_perms p on (a.id = p.id) 
+	FULL OUTER join app_packages pkg  on (a.id = pkg.id)  order by max_installs;
+
+create index apps_title_gin_trgm_idx on matview.apps_play_data USING GIN (title gin_trgm_ops);
 
 grant select on matview.apps_play_data to apiserv;
 
